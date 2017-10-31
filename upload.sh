6e4673bbfd727c4a2b608f86a39e0cbc9b5224aa
homma <daisuke.homma@gmail.com>
2017-10-31 23:37:53 +0900

    committed on 2017/10/31 23:37.

--- upload.sh
+++ upload.sh
@@ -5,18 +5,22 @@
 ##
 ## to create a repository
 ##
-## create repo from github.com, click '+' menu to create new repository.
-## then run below in the project directory.
+## create a repo at the github.com by clicking '+' menu.
+## then run following commands in the project directory.
+##
 ## git init
 ## git commit -m 'first commit'
 ## git remote add origin https://github.com/<user>/<project>.git
 ## git push -u origin master
+##
 ## having done, then run this script.
 ##
 ## to clone a repository
 ##
-## check the repository url from the [Clone or download] button
+## check the repo url at the github.com from the [Clone or download] button.
+##
 ## git clone https://github.com/<user>/<project>.git
+##
 ## modify files and run upload.sh
 ##
 
